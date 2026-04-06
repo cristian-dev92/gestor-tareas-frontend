804453be446a7bc08b0602a05c489acd474b4f7f
Cristian Alhambra <cristianalh@gmail.com>
2026-04-06 19:45:26 +0200

    Add files via upload

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, CommonModule, RouterModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+  username: string = '';
+  password: string = '';
+  errormessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    const credentials = { 
+      username: this.username, 
+      password: this.password 
+    };
+
+    this.authService.login(credentials).subscribe({
+      next: (response) => {
+        this.authService.saveToken(response.token);
+        this.router.navigate(['/tasks']);
+      },
+      error: () => {
+        this.errormessage = 'Username or password is incorrect. Please try again.';
+      }
+    });
+  }
+}
