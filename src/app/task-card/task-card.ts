8b68cd5f6836269fa54a3c86a436f46c8b43ac68
Cristian Alhambra <cristianalh@gmail.com>
2026-04-06 19:43:19 +0200

    Add files via upload

---
+++ src/app/task-card/task-card.ts
@@ -0,0 +1,66 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-task-card',
+  standalone: true,
+  templateUrl: './task-card.html',
+  styleUrl: './task-card.css',
+  imports: [CommonModule, FormsModule, DatePipe]
+})
+export class TaskCardComponent {
+
+  @Input() task: any;
+
+  @Output() toggleTask = new EventEmitter<any>();
+  @Output() toggleSubtask = new EventEmitter<any>();
+  @Output() deleteTask = new EventEmitter<number>();
+  @Output() deleteSubtask = new EventEmitter<number>();
+  @Output() editTask = new EventEmitter<any>();
+  @Output() addSubtask = new EventEmitter<{ task: any, title: string }>();
+
+
+  newSubtaskText: string = '';
+
+  onToggleTask() {
+    this.toggleTask.emit(this.task);
+  }
+
+  onToggleSub(sub: any) {
+    this.toggleSubtask.emit(sub);
+  }
+
+  onDeleteTask() {
+    this.deleteTask.emit(this.task.id);
+  }
+
+  onDeleteSub(id: number) {
+    this.deleteSubtask.emit(id);
+  }
+
+  onEditTask() {
+    this.editTask.emit(this.task);
+  }
+
+  onAddSubtask() {
+    if (!this.newSubtaskText.trim()) return;
+    this.addSubtask.emit({ task: this.task, title: this.newSubtaskText });
+    this.newSubtaskText = '';
+  }
+
+  translatePriority(priority: string): string {
+    switch (priority.toLowerCase()) {
+      case 'low':
+        return 'Baja';
+      case 'medium':
+        return 'Media';
+      case 'high':
+        return 'Alta';
+      default:
+        return priority;
+    } 
+  }
+
+}
