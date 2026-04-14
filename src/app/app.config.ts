5ebdd5e7e9adf07cfcdf38b3bbc1ffc202ce2091
Cristian Alhambra <cristianalh@gmail.com>
2026-04-14 13:47:12 +0200

    Add variables run to vercel

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient,withInterceptorsFromDi,HTTP_INTERCEPTORS} from '@angular/common/http';
 import { routes } from './app.routes';
--- src/app/services/task.ts
+++ src/app/services/task.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  private apiUrl = 'http://localhost:8080/tasks';
+  private apiUrl = `${environment.apiUrl}/tasks`;
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
